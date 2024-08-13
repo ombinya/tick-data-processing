d43f59e0086d0405f1e6f5d48ff8242868560fb9
Paul Muoga <paul.sofdev@gmail.com>
2024-08-13 16:24:12 +0300

    Modified test_db_file_exists to cater for false file path

--- tests/testrefiner.py
+++ tests/testrefiner.py
@@ -6,14 +6,19 @@ import os
 class TestRefiner(unittest.TestCase):
     def setUp(self):
         # Directory of the current script
-        current_dir = os.path.dirname(os.path.abspath(__file__))
+        self.current_dir = os.path.dirname(os.path.abspath(__file__))
         # Construct the path to the db file
-        self.dbfilepath = os.path.join(current_dir, '..', 'data', 'eurusd.db')
+        self.dbfilepath = os.path.join(self.current_dir, '..', 'data', 'eurusd.db')
 
         self.refiner = Refiner(self.dbfilepath)
 
     def test_db_file_exists(self):
         self.assertTrue(self.refiner.db_file_exists())
+
+        falsefilepath = os.path.join(self.current_dir, '..', 'data', 'falsefile.db')
+        self.refiner.dbfilepath = falsefilepath
+
+        self.assertFalse(self.refiner.db_file_exists())
 
 
 if __name__ == "__main__":
