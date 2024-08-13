415b20613d013d8be6d8f598fdb20516e0613d99
Paul Muoga <paul.sofdev@gmail.com>
2024-08-13 16:11:58 +0300

    Added test_db_file_exists and db_file_exists

--- refiner.py
+++ refiner.py
@@ -5,10 +5,14 @@ import os
 class Refiner:
     def __init__(self, dbfilepath):
         self.dbfilepath = dbfilepath
+        # Each segment is 10 minutes (600 seconds) long; I need at least half number (300) of ticks
+        self.minimumsegmentsize = (10 * 60) / 2
 
     def db_file_exists(self):
         return os.path.exists(self.dbfilepath)
 
 
 if __name__ == "__main__":
-    refiner = Refiner()
+    dbfilepath = "data/eurusd.db"
+    refiner = Refiner(dbfilepath)
+
--- tests/testrefiner.py
+++ tests/testrefiner.py
@@ -1,14 +1,19 @@
 import unittest
 from ..refiner import Refiner
+import os
 
 
 class TestRefiner(unittest.TestCase):
     def setUp(self):
-        self.dbfilename = "somedb.db"
-        refiner = Refiner(self.dbfilename)
+        # Directory of the current script
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        # Construct the path to the db file
+        self.dbfilepath = os.path.join(current_dir, '..', 'data', 'eurusd.db')
 
-    def test_db_file_exists():
-        return False
+        self.refiner = Refiner(self.dbfilepath)
+
+    def test_db_file_exists(self):
+        self.assertTrue(self.refiner.db_file_exists())
 
 
 if __name__ == "__main__":
